796ea119efe7fac5b385afdc42b1964cf6aff487
Brooke Ryan <bkryan@ucsd.edu>
2015-11-02 09:30:25 -0800

    Added Success Toasts
    Added messages indicating “Add User Successful” and “Invite User
    Successful”

--- APPpartment2/app/src/main/java/net/brooke/apppartment2/AddExistingUserActivity.java
+++ APPpartment2/app/src/main/java/net/brooke/apppartment2/AddExistingUserActivity.java
@@ -4,6 +4,8 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Toast;
+import android.content.Context;
 
 public class AddExistingUserActivity extends AppCompatActivity {
 
@@ -14,6 +16,13 @@ public class AddExistingUserActivity extends AppCompatActivity {
     }
 
     public void dashboard(View v) {
+        Context context = getApplicationContext();
+        CharSequence text = "Add User Successful!";
+        int duration = Toast.LENGTH_SHORT;
+
+        Toast toast = Toast.makeText(context, text, duration);
+        toast.show();
+
         Intent intent = new Intent(AddExistingUserActivity.this, Dashboard_w_NavDrawer.class);
         startActivity(intent);
     }
--- APPpartment2/app/src/main/java/net/brooke/apppartment2/SendInviteActivity.java
+++ APPpartment2/app/src/main/java/net/brooke/apppartment2/SendInviteActivity.java
@@ -1,9 +1,11 @@
 package net.brooke.apppartment2;
 
+import android.content.Context;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Toast;
 
 public class SendInviteActivity extends AppCompatActivity {
 
@@ -14,6 +16,13 @@ public class SendInviteActivity extends AppCompatActivity {
     }
 
     public void dashboard(View v) {
+        Context context = getApplicationContext();
+        CharSequence text = "Invite User Successful!";
+        int duration = Toast.LENGTH_SHORT;
+
+        Toast toast = Toast.makeText(context, text, duration);
+        toast.show();
+
         Intent intent = new Intent(SendInviteActivity.this, Dashboard_w_NavDrawer.class);
         startActivity(intent);
     }
