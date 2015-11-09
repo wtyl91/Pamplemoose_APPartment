01baff49eddb2788ff96de79368ceea55596c5ce
Sean Wenzel <sdwenzel@ucsd.edu>
2015-11-08 17:07:24 -0800

    Apartment Membership Check
    Implemented a check for whether a user already exists in an apartment,
    or if they will have to create a new apartment or request to join
    somebody else’s apartment.

--- APPpartment2/app/src/main/java/net/brooke/apppartment2/MainActivity.java
+++ APPpartment2/app/src/main/java/net/brooke/apppartment2/MainActivity.java
@@ -12,25 +12,22 @@ import android.content.Intent;
 
 import com.parse.Parse;
 import com.parse.ParseObject;
+import com.parse.ParseQuery;
+import com.parse.ParseUser;
 import com.parse.ui.ParseLoginBuilder;
 import com.parse.ParseFacebookUtils;
+
+import java.util.ArrayList;
 
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-
-
         super.onCreate(savedInstanceState);
 
         ParseLoginBuilder builder = new ParseLoginBuilder(MainActivity.this);
         startActivityForResult(builder.build(), 0);
-
-
-
-
-
     }
 
   /*  @Override
@@ -61,8 +58,21 @@ public class MainActivity extends AppCompatActivity {
         ParseFacebookUtils.onActivityResult(requestCode, resultCode, data);
 
         if (resultCode == RESULT_OK) {
-            Intent intent = new Intent(MainActivity.this, Dashboard_w_NavDrawer.class);
-            startActivity(intent);
+            ParseUser user = ParseUser.getCurrentUser();
+            String household = user.getString("household");
+
+            // Is this equivalent to undefined?
+            if (household == null) {
+                // Failure: Need to create new apartment or choose existing one
+                Intent intentFailure = new Intent(MainActivity.this, EnterApartmentActivity.class);
+                startActivity(intentFailure);
+            }
+            else {
+                // Success: Send to Dashboard
+                Intent intentSuccess = new Intent(MainActivity.this, Dashboard_w_NavDrawer.class);
+                startActivity(intentSuccess);
+            }
+
         }
     }
 }
