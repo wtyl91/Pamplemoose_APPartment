309e107c70c27fa55ca2d76b947d405462a79ce3
mandy shieh <m3shieh@ucsd.edu>
2015-11-12 22:22:03 -0800

    Displays roommate name on dashbaord

--- APPpartment2/app/src/main/java/net/brooke/apppartment2/Dashboard_w_NavDrawer.java
+++ APPpartment2/app/src/main/java/net/brooke/apppartment2/Dashboard_w_NavDrawer.java
@@ -17,6 +17,7 @@ import android.view.MenuItem;
 import android.widget.TextView;
 
 import com.parse.GetCallback;
+import com.parse.FindCallback;
 import com.parse.Parse;
 import com.parse.ParseException;
 import com.parse.ParseObject;
@@ -24,6 +25,7 @@ import com.parse.ParseQuery;
 import com.parse.ParseUser;
 
 import java.util.ArrayList;
+import java.util.List;
 
 
 public class Dashboard_w_NavDrawer extends AppCompatActivity
@@ -34,80 +36,73 @@ public class Dashboard_w_NavDrawer extends AppCompatActivity
         // Retrieves the current user's apartment code string
         ParseUser user = ParseUser.getCurrentUser();
         String aptCode = user.getString("household");
+        
+        ParseQuery<ParseUser> usersQuery = ParseUser.getQuery();
+        usersQuery.whereEqualTo("household", aptCode);
 
-        // Retrieves the household associated with the current user's apartment
-        ParseQuery<ParseObject> household = ParseQuery.getQuery("Household");
-        household.whereEqualTo("houseID", aptCode);
+        usersQuery.findInBackground(new FindCallback<ParseUser>() {
+            public void done(List<ParseUser> liverList, ParseException e) {
+                if (e == null) {
 
-        household.getFirstInBackground(new GetCallback<ParseObject>() {
-            public void done(ParseObject object, ParseException e) {
-                if (object == null) {
-                    System.out.println("Error: Could not find apartment.");
-                } else {
-                    // Gets the inhabitants array from Household object
-                    ArrayList<String> inhabitants= (ArrayList<String>) object.get("inhabitants");
-                    ParseQuery<ParseObject> users = ParseQuery.getQuery("User");
+                    int size = liverList.size();
 
-                    int size = inhabitants.size();
-                    ParseQuery<ParseObject> username;
-
-                    // TODO: I have no idea how to get the string version of username
-                    // Anyone know?
-                    username = users.whereEqualTo("username", inhabitants.get(0));
-
-                    // Prints out inhabitants to the TextViews
                     if (size > 0) {
                         TextView myTextView1 = (TextView) findViewById(R.id.Person1);
-                        myTextView1.setText(inhabitants.get(0));
+                        myTextView1.setText(liverList.get(0).getString("name"));
                     }
 
                     if (size > 1) {
                         TextView myTextView2 = (TextView) findViewById(R.id.Person2);
-                        myTextView2.setText(inhabitants.get(1));
+                        myTextView2.setText(liverList.get(1).getString("name"));
                     }
 
                     if (size > 2) {
                         TextView myTextView3 = (TextView) findViewById(R.id.Person3);
-                        myTextView3.setText(inhabitants.get(2));
+                        myTextView3.setText(liverList.get(2).getString("name"));
                     }
 
                     if (size > 3) {
                         TextView myTextView4 = (TextView) findViewById(R.id.Person4);
-                        myTextView4.setText(inhabitants.get(3));
+                        myTextView4.setText(liverList.get(3).getString("name"));
                     }
 
                     if (size > 4) {
                         TextView myTextView5 = (TextView) findViewById(R.id.Person5);
-                        myTextView5.setText(inhabitants.get(4));
+                        myTextView5.setText(liverList.get(4).getString("name"));
                     }
 
                     if (size > 5) {
                         TextView myTextView5 = (TextView) findViewById(R.id.Person6);
-                        myTextView5.setText(inhabitants.get(5));
+                        myTextView5.setText(liverList.get(5).getString("name"));
                     }
 
                     if (size > 6) {
                         TextView myTextView5 = (TextView) findViewById(R.id.Person7);
-                        myTextView5.setText(inhabitants.get(6));
+                        myTextView5.setText(liverList.get(6).getString("name"));
                     }
 
                     if (size > 7) {
                         TextView myTextView5 = (TextView) findViewById(R.id.Person8);
-                        myTextView5.setText(inhabitants.get(7));
+                        myTextView5.setText(liverList.get(7).getString("name"));
                     }
 
                     if (size > 8) {
                         TextView myTextView5 = (TextView) findViewById(R.id.Person9);
-                        myTextView5.setText(inhabitants.get(8));
+                        myTextView5.setText(liverList.get(8).getString("name"));
                     }
 
                     if (size > 9) {
                         TextView myTextView5 = (TextView) findViewById(R.id.Person10);
-                        myTextView5.setText(inhabitants.get(9));
+                        myTextView5.setText(liverList.get(9).getString("name"));
                     }
+
+
+                } else {
+                    Log.d("roommates", "Error: " + e.getMessage());
                 }
             }
         });
+
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dashboard_w__nav_drawer);
