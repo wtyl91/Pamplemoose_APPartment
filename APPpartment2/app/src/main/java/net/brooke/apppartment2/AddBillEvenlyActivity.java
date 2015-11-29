ea5ae47d39dbd847a19c772642a17f528aca8230
Sean Wenzel <sdwenzel@ucsd.edu>
2015-11-29 12:50:33 -0800

    Sean, the Underdog Story: Episode 2 - "Sean spends 3 days playing with a listView"
    Custom Split almost works! I’ll finish it later.

--- APPpartment2/app/src/main/java/net/brooke/apppartment2/AddBillEvenlyActivity.java
+++ APPpartment2/app/src/main/java/net/brooke/apppartment2/AddBillEvenlyActivity.java
@@ -27,6 +27,7 @@ import com.parse.ParseException;
 import com.parse.ParseObject;
 import com.parse.ParseQuery;
 import com.parse.ParseUser;
+import com.parse.SaveCallback;
 
 import java.lang.reflect.Array;
 import java.util.ArrayList;
@@ -52,11 +53,23 @@ public class AddBillEvenlyActivity extends AppCompatActivity {
         final String titleStr = title.getText().toString();
         final String descriptionStr = description.getText().toString();
 
+        if (titleStr.equals("")) {
+            Toast.makeText(getApplicationContext(), "Error: Please enter name of bill.",
+                    Toast.LENGTH_LONG).show();
+            return;
+        }
+
+        if (code.equals("")) {
+            Toast.makeText(getApplicationContext(), "Error: Please enter total amount of bill.",
+                    Toast.LENGTH_LONG).show();
+            return;
+        }
+
         double amount = Double.parseDouble(code);
         System.out.println("Total Amount: " + amount);
 
         // Assume checkboxes returned an array of liverNums called numList
-        ParseObject newbill = new ParseObject("Bills");
+        final ParseObject newbill = new ParseObject("Bills");
         newbill.put("Creator", ParseUser.getCurrentUser().getString("username"));
         newbill.put("TotalAmount", amount);
         newbill.put("Description", descriptionStr);
@@ -78,17 +91,6 @@ public class AddBillEvenlyActivity extends AppCompatActivity {
 
             if (btnSwitch.isChecked() == false) {
 
-                /* // OLD VERSION
-            for (Number num : numList) {
-
-                String tagName = "inhabitant" + num.toString();
-
-                if (num == ParseUser.getCurrentUser().getNumber("liverNum")) {
-                    newbill.put(tagName, (amount - splitAmount) * (-1));
-                } else {
-                    newbill.put(tagName, splitAmount);
-                }
-                countLiver++; */
 
                 String tagName;
                 for (Number num : numList) {
@@ -103,22 +105,19 @@ public class AddBillEvenlyActivity extends AppCompatActivity {
         // Else, if switch is On, use Custom Split
         else {
             Double sum = 0.0;
-                double[] splitArray = new double[numList.length];
+                //double[] splitArray = new double[numList.length];
 
-            for(int i = 0; i < listView.getAdapter().getCount(); i++){
-                View view = listView.getChildAt(i);
-                EditText editText = (EditText) view.findViewById(R.id.et);
-                String string = editText.getText().toString();
-                if(!string.equals("")) {
-                    splitArray[i] = Double.parseDouble(string);
-                    sum += Double.parseDouble(string);
+                String[] amounts = ((CustomAdapter)listView.getAdapter()).getAmounts();
+
+                for (int i = 0; i < amounts.length; i++) {
+                    if (amounts[i].equals("")) {
+                        Toast.makeText(getApplicationContext(), "Error: Not all fields have been filled out.",
+                                Toast.LENGTH_LONG).show();
+                        return;
+                    }
+                    sum += Double.parseDouble(amounts[i]);
                 }
-                else {
-                    Toast.makeText(getApplicationContext(), "Error: Not all fields have been filled out.",
-                            Toast.LENGTH_LONG).show();
-                    return;
-                }
-            }
+                System.out.println("Sum is: " + sum);
 
 
             //System.out.println("Sum is: " + sum);
@@ -133,7 +132,7 @@ public class AddBillEvenlyActivity extends AppCompatActivity {
                 for (Number num : numList) {
 
                     tagName = "inhabitant" + num.toString();
-                    newbill.put(tagName, splitArray[index]);
+                    newbill.put(tagName, Double.parseDouble(amounts[index]));
                     index++;
                 }
                 tagName = "inhabitant" + ParseUser.getCurrentUser().getNumber("liverNum").toString();
@@ -142,90 +141,95 @@ public class AddBillEvenlyActivity extends AppCompatActivity {
 
         }
 
-        newbill.saveInBackground();
-
-        Intent intent = new Intent(AddBillEvenlyActivity.this,Dashboard_w_NavDrawer.class);
-        startActivity(intent);
-        Toast.makeText(getApplicationContext(), "Successfully added bill.", Toast.LENGTH_SHORT).show();
-
-    }
-
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_add_bill_evenly);
-
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
-
-        getSupportActionBar().setTitle(null);
-        getSupportActionBar().setLogo(R.drawable.appartment_logo_red);
-
-
-        extras = getIntent().getExtras();
-        numList = extras.getIntArray("numList");
-
-        for (int i = 0; i < numList.length; i++) {
-            System.out.println("numList: " + numList[i]);
-        }
-
-        setupListView();
-        for (int i = 0; i < numList.length; i++) {
-            System.out.println("num: " + numList[i]);
-        }
-    }
-
-    private void setupListView() {
-
-        final ListView listView = (ListView) findViewById(R.id.listView);
-
-        String aptCode = ParseUser.getCurrentUser().getString("household");
-
-        // Make a list of queries
-        List<ParseQuery<ParseUser>> queries = new ArrayList<ParseQuery<ParseUser>>();
-
-
-        for (int i = 0; i < numList.length; i++) {
-            ParseQuery<ParseUser> userQuery = ParseUser.getQuery();
-            userQuery.whereEqualTo("household", aptCode);
-            userQuery.whereEqualTo("liverNum", numList[i]);
-
-            queries.add(userQuery);
-        }
-        ParseQuery<ParseUser> mainQuery = ParseQuery.or(queries);
-
-        mainQuery.findInBackground(new FindCallback<ParseUser>() {
-            public void done(List<ParseUser> users, ParseException e) {
-                String[] names = new String[numList.length];
-
-                for (int i = 0; i < names.length; i++) {
-                    names[i] = users.get(i).getString("name");
-                }
-                listView.setItemsCanFocus(true);
-                //ArrayAdapter<String> aa=new ArrayAdapter<String>(AddBillEvenlyActivity.this, android.R.layout.simple_spinner_item,
-                //        names);
-
-                listView.setAdapter(new CustomAdapter(AddBillEvenlyActivity.this, names));
-            }
-        });
-
-        // Listen to Switch
-        Switch btnSwitch = (Switch) findViewById(R.id.switch1);
-        btnSwitch.setChecked(false);
-
-        btnSwitch.setOnCheckedChangeListener(new OnCheckedChangeListener() {
+        newbill.saveInBackground(new SaveCallback() {
             @Override
-            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
-                if (isChecked) {
-                    //Log.i("Switch", "ON");
-                    listView.setVisibility(View.VISIBLE);
+            public void done(ParseException e) {
+                newbill.saveInBackground();
 
-                } else {
-                    //Log.i("Switch", "OFF");
-                    listView.setVisibility(View.INVISIBLE);
-                }
+                Intent intent = new Intent(AddBillEvenlyActivity.this, Dashboard_w_NavDrawer.class);
+                startActivity(intent);
+                Toast.makeText(getApplicationContext(), "Successfully added bill.", Toast.LENGTH_SHORT).show();
+
             }
         });
     }
-}
+
+
+            @Override
+            protected void onCreate(Bundle savedInstanceState) {
+                super.onCreate(savedInstanceState);
+                setContentView(R.layout.activity_add_bill_evenly);
+
+                Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+                setSupportActionBar(toolbar);
+
+                getSupportActionBar().setTitle(null);
+                getSupportActionBar().setLogo(R.drawable.appartment_logo_red);
+
+
+                extras = getIntent().getExtras();
+                numList = extras.getIntArray("numList");
+
+                for (int i = 0; i < numList.length; i++) {
+                    System.out.println("numList: " + numList[i]);
+                }
+
+                setupListView();
+                for (int i = 0; i < numList.length; i++) {
+                    System.out.println("num: " + numList[i]);
+                }
+            }
+
+            private void setupListView() {
+
+                final ListView listView = (ListView) findViewById(R.id.listView);
+
+                String aptCode = ParseUser.getCurrentUser().getString("household");
+
+                // Make a list of queries
+                List<ParseQuery<ParseUser>> queries = new ArrayList<ParseQuery<ParseUser>>();
+
+
+                for (int i = 0; i < numList.length; i++) {
+                    ParseQuery<ParseUser> userQuery = ParseUser.getQuery();
+                    userQuery.whereEqualTo("household", aptCode);
+                    userQuery.whereEqualTo("liverNum", numList[i]);
+
+                    queries.add(userQuery);
+                }
+                ParseQuery<ParseUser> mainQuery = ParseQuery.or(queries);
+
+                mainQuery.findInBackground(new FindCallback<ParseUser>() {
+                    public void done(List<ParseUser> users, ParseException e) {
+                        String[] names = new String[numList.length];
+
+                        for (int i = 0; i < names.length; i++) {
+                            names[i] = users.get(i).getString("name");
+                        }
+                        listView.setItemsCanFocus(true);
+                        //ArrayAdapter<String> aa=new ArrayAdapter<String>(AddBillEvenlyActivity.this, android.R.layout.simple_spinner_item,
+                        //        names);
+
+                        listView.setAdapter(new CustomAdapter(AddBillEvenlyActivity.this, names));
+                    }
+                });
+
+                // Listen to Switch
+                Switch btnSwitch = (Switch) findViewById(R.id.switch1);
+                btnSwitch.setChecked(false);
+
+                btnSwitch.setOnCheckedChangeListener(new OnCheckedChangeListener() {
+                    @Override
+                    public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                        if (isChecked) {
+                            //Log.i("Switch", "ON");
+                            listView.setVisibility(View.VISIBLE);
+
+                        } else {
+                            //Log.i("Switch", "OFF");
+                            listView.setVisibility(View.INVISIBLE);
+                        }
+                    }
+                });
+            }
+        }
--- APPpartment2/app/src/main/java/net/brooke/apppartment2/CustomAdapter.java
+++ APPpartment2/app/src/main/java/net/brooke/apppartment2/CustomAdapter.java
@@ -5,6 +5,8 @@ package net.brooke.apppartment2;
  */
 
 import android.content.Context;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.View.OnClickListener;
@@ -15,17 +17,23 @@ import android.widget.TextView;
 import android.widget.Toast;
 public class CustomAdapter extends BaseAdapter {
     String [] result;
+    private String[] amounts;
     Context context;
     //int [] imageId;
     private static LayoutInflater inflater=null;
     public CustomAdapter(AddBillEvenlyActivity mainActivity, String[] nameList) {
         // TODO Auto-generated constructor stub
         result=nameList;
+        amounts = new String[result.length];
         context=mainActivity;
-        //imageId=prgmImages;
         inflater = ( LayoutInflater )context.
                 getSystemService(Context.LAYOUT_INFLATER_SERVICE);
     }
+
+    public String[] getAmounts() {
+        return amounts;
+    }
+
     @Override
     public int getCount() {
         // TODO Auto-generated method stub
@@ -35,7 +43,7 @@ public class CustomAdapter extends BaseAdapter {
     @Override
     public Object getItem(int position) {
         // TODO Auto-generated method stub
-        return position;
+        return amounts[position];
     }
 
     @Override
@@ -44,29 +52,58 @@ public class CustomAdapter extends BaseAdapter {
         return position;
     }
 
-    public class Holder
+    public class ViewHolder
     {
-        TextView tv;
-        EditText et;
+        public TextView tv;
+        public EditText et;
+        public TextWatcher tw;
     }
+
     @Override
     public View getView(final int position, View convertView, ViewGroup parent) {
         // TODO Auto-generated method stub
-        Holder holder=new Holder();
-        View rowView;
-        rowView = inflater.inflate(R.layout.listitems, null);
-        holder.tv=(TextView) rowView.findViewById(R.id.tv);
-        holder.et=(EditText) rowView.findViewById(R.id.et);
-        holder.tv.setText(result[position]);
-        //holder.et.setImageResource(imageId[position]);
-        /*
-        rowView.setOnClickListener(new OnClickListener() {
+        //final ViewHolder holder;
+        View rowView = convertView;
+        //View rowView;
+        if (rowView == null) {
+            //System.out.println("convertView was null: position = " + position);
+            ViewHolder holder = new ViewHolder();
+            rowView = inflater.inflate(R.layout.listitems, parent, false);
+            holder.tv = (TextView) rowView.findViewById(R.id.tv);
+            holder.et = (EditText) rowView.findViewById(R.id.et);
+
+            rowView.setTag(holder);
+
+        }
+
+            ViewHolder holder = (ViewHolder) rowView.getTag();
+
+
+        if (holder.tw != null)
+            holder.et.removeTextChangedListener(holder.tw);
+
+
+
+        // Keep a reference to the TextWatcher so that we can remove it later
+        holder.tw = new TextWatcher() {
             @Override
-            public void onClick(View v) {
-                // TODO Auto-generated method stub
-                Toast.makeText(context, "You Clicked "+result[position], Toast.LENGTH_LONG).show();
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
             }
-        }); */
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                amounts[position] = s.toString();
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+            }
+        };
+        holder.et.addTextChangedListener(holder.tw);
+
+        holder.tv.setText(result[position]);
+        holder.et.setText(amounts[position]);
+
         return rowView;
     }
 
